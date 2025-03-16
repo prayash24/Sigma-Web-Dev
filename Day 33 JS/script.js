0857af550bf163fa990943864958615f2f8e9615
prayash24 <prayashnaik24@gmail.com>
2025-03-16 10:29:27 +0530

    Create script.js

---
+++ Day 33 JS/script.js
@@ -0,0 +1,17 @@
+//variable and constants
+
+var a = 12;
+//12 is stored under a container 'a'
+
+const b = 13; //const is constant and its value can not be changed
+
+//compilers and interpreters - js use english lang and computer can not understand they us=nderstand 0 1 as signals 
+// - and to convert our lang to computer understandable lang we use a translator
+// - compilers and interpreter are the translator to convert the code
+// - compiler - convert to machine code (faster code) and compiler has slower conversion rate
+// - interpreter - convert to byte code (slower code) and interpretation has faster conversion rate 
+//               - byte code again get converted to machine code
+// js has inbuilt interpreter
+// google has developed jit compiler (just in time) - it use the ability of compiler and interpreter
+
+//window - collection of all the tools 
