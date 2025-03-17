b64ca1be5d01fae5c08ec6d7dedc973884fc9e3a
prayash24 <prayashnaik24@gmail.com>
2025-03-18 00:51:38 +0530

    Update script.js

---
+++ Day 34/script.js
@@ -0,0 +1,39 @@
+//types 
+// types are of 2 types 
+//      1- Premitive
+//      2- Reference - [] {} () example of reference data types
+// float - 21.2
+// string - harsh
+// boolean - true/false
+
+//in reference data type when u make a copy of a variable and try to edit the value of that copied variable the changes are also reflect on the original variable to get rid of this we use spead operator
+//spread operator
+var ar = [12, 13, 14, 15];
+var ba = [...ar];
+
+//conditionals
+//  1- if else
+//  2- else if
+//  3- ternary operator
+//  4- switch case
+
+//truthy and falsy values - every value can be converted into true or false 
+// null undefined NaN 0 "" '' document.all false - except this every value is true
+var age = 18;
+if(age>18){
+    console.log("Hey");
+}
+else{
+    console.log("Hello");
+}
+
+//loops - for while do-while foreach forin forof
+for(var i = 5; num<10; i++){
+
+}
+
+//while loop
+start;
+while(end){
+    change;
+}
